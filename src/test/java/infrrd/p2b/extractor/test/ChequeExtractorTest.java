45ce8cd4e536e611efc04144db9dca2fdd20871a
pallavichand <pallavi.chand@chromeriver.com>
2018-09-26 14:25:03 +0530

    Adding P2B to new Repo

--- src/test/java/infrrd/p2b/extractor/test/ChequeExtractorTest.java
+++ src/test/java/infrrd/p2b/extractor/test/ChequeExtractorTest.java
@@ -13,12 +13,12 @@ public class ChequeExtractorTest {
     @Test
     public void testExtractDefault() throws IOException
     {
-       // String filePath = " /home/pallavi/work/Repos/POC/Cheques_Tesseract_o:p/";
+       String filePath = " /home/pallavi/work/Repos/POC/Cheques_Tesseract_o:p/";
     	
     	//String filePath = "/home/pallavi/work/Repos/POC/Remittances/Remittances/";
-    	String filePath = "/home/pallavi/work/POC/P2B/Examples for Infrrd/Examples for Infrrd/";
+    	//String filePath = "/home/pallavi/work/POC/P2B/Examples for Infrrd/Examples for Infrrd/";
     	//Cheques -------------------------------------------------------------------------------------
-       // String fileName = "2D_LB_DOCS_9.19.17-page-1-1.txt"; //143.36, 0000995638, Absolute Security Systems Inc, 2d Electronics, september 14, 2017
+        String fileName = "2D_LB_DOCS_9.19.17-page-1-1.txt"; //143.36, 0000995638, Absolute Security Systems Inc, 2d Electronics, september 14, 2017
     //String fileName = "7MED_LB_DOCS_9.19.17-page-1-1.txt"; //3946.51, 0000975300,Chiro Med Health Center Inc, Softwise, september 12, 2017
       // String fileName = "70KFT_LB_DOCS_9.19.17-page-1-1.txt"; //7346.44, 57877,Numed, Inc, No text Found (70Kft), sep 11 2017
       //  String fileName = "AGENCY_LB_DOCS_9.18.17-page-1-1.txt"; //1861.50, 18688, Insurance Aquisitions Inc (Text not found), Agengy Revolution Of C/o P2binvestor, Inc Y, sep 7,2017
@@ -28,7 +28,7 @@ public class ChequeExtractorTest {
      // String fileName = "SPECCO_LB_DOCS_9.19.17-page-5-1.txt";//820.71, 0282847, Stetson Building Products, Llc, Specco Industries, 09/12/17
      // String fileName = "SPECCO_LB_DOCS_9.19.17-page-7-1.txt"; //742.50, 862796,Siteone Landscape Supply ,Specco Industries, 9/8/2017
      //String fileName = "SPECCO_LB_DOCS_9.19.17-page-12-1.txt"; //495.31,038472, Specco Industries, Inc, 09/11/17
-    	String fileName ="page-1.txt";
+    //	String fileName ="page-1.txt";
      //Remmitences --------------------------------------------------------------------------------------------
      //String fileName = "2D_LB_DOCS_9.19.17-page-2-1.txt"; 
    //  String fileName = "70KFT_LB_DOCS_9.19.17-page-2-1.txt";
@@ -41,9 +41,9 @@ public class ChequeExtractorTest {
         DocumentServiceImpl documentService = new DocumentServiceImpl();
         Map<String, String> match = documentService.getTextByPathToTest( filePath+fileName ,  "chk");
         log.info("Amount Found  -->  {}", match.get("Amount"));
-        log.info("Cheque Number Found  -->  {}",  match.get("ChequeNumber"));
-        log.info("Payor Found  --> {}",  match.get("Payor"));
+        log.info("Cheque Number Found  -->  {}",  match.get("Cheque Number"));
+        log.info("Payor Found  --> {}",  match.get("Payer"));
         log.info("Payee Found  --> {}",  match.get("Payee"));
-        log.info("Bill Date Found  --> {}",  match.get("BillDate"));
+        log.info("Bill Date Found  --> {}",  match.get("Cheque Date"));
     }
 }
